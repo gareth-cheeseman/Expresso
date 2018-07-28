6f3ad48c4de96c268560af3df086e211d75ff9a0
gareth-cheeseman <garethcheeseman@humbug.local>
2018-07-28 14:58:20 +0100

    Adds employee routes

--- migration.js
+++ migration.js
@@ -3,36 +3,36 @@ const db = new sqlite3.Database('./database.sqlite');
 
 db.serialize(() => {
   db.run(
-    `CREATE TABLE IF NOT EXISTS Employee (
+    `CREATE TABLE IF NOT EXISTS Employee(
       id INTEGER PRIMARY KEY NOT NULL,
       name TEXT NOT NULL,
       position TEXT NOT NULL,
       wage INTEGER NOT NULL,
-      is_current_employee INTEGER NOT NULL DEFAULT 1)`
+      is_current_employee INTEGER NOT NULL DEFAULT 1);`
   );
 
   db.run(
-    `CREATE TABLE IF NOT EXISTS Timesheet (
+    `CREATE TABLE IF NOT EXISTS Timesheet(
       id INTEGER PRIMARY KEY NOT NULL,
       hours INTEGER NOT NULL,
       rate INTEGER NOT NULL, 
       date INTEGER NOT NULL,
-      employee_id INTEGER REFERENCES Employee(id) NOT NULL)`
+      employee_id INTEGER REFERENCES Employee(id) NOT NULL);`
   );
 
   db.run(
-    `CREATE TABLE IF NOT EXISTS Menu (
+    `CREATE TABLE IF NOT EXISTS Menu(
       id INTEGER PRIMARY KEY NOT NULL,
-      title TEXT NOT NULL)`
+      title TEXT NOT NULL);`
   );
 
   db.run(
-    `CREATE TABLE IF NOT EXISTS MenuItem (
+    `CREATE TABLE IF NOT EXISTS MenuItem(
       id INTEGER PRIMARY KEY NOT NULL,
       name TEXT NOT NULL,
       description TEXT NOT NULL, 
       inventory INTEGER NOT NULL,
       price INTEGER NOT NULL,
-      menu_id INTEGER REFERENCES Menu(id) NOT NULL)`
+      menu_id INTEGER REFERENCES Menu(id) NOT NULL);`
   );
 });
